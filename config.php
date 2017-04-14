a9652e9ff4deb81da13160bbe50203526f777aef
Netping <sp@netping.ru>
2017-04-14 22:01:40 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://npsite.azurewebsites.net/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://npsite.azurewebsites.net/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/home/site/wwwroot/catalog/');
+define('DIR_SYSTEM', 'D:/home/site/wwwroot/system/');
+define('DIR_IMAGE', 'D:/home/site/wwwroot/image/');
+define('DIR_LANGUAGE', 'D:/home/site/wwwroot/catalog/language/');
+define('DIR_TEMPLATE', 'D:/home/site/wwwroot/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/home/site/wwwroot/system/config/');
+define('DIR_CACHE', 'D:/home/site/wwwroot/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/home/site/wwwroot/system/storage/download/');
+define('DIR_LOGS', 'D:/home/site/wwwroot/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/home/site/wwwroot/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/home/site/wwwroot/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'eu-cdbr-azure-west-d.cloudapp.net');
+define('DB_USERNAME', 'be570f71559f78');
+define('DB_PASSWORD', '3e99587e');
+define('DB_DATABASE', 'netpingopencartdb');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
