6d331b4355cca5d3f16cfd5e2f1da9829e496edb
Netping <sp@netping.ru>
2017-04-14 19:39:04 +0600

    Test_change_2

--- admin/config.php
+++ admin/config.php
@@ -1,29 +1,29 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://netpingdevice.localhost/admin/');
-define('HTTP_CATALOG', 'http://netpingdevice.localhost/');
+define('HTTP_SERVER', 'http://npsite.azurewebsites.net/admin/');
+define('HTTP_CATALOG', 'http://npsite.azurewebsites.net/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://netpingdevice.localhost/admin/');
-define('HTTPS_CATALOG', 'http://netpingdevice.localhost/');
+define('HTTPS_SERVER', 'http://npsite.azurewebsites.net/admin/');
+define('HTTPS_CATALOG', 'http://npsite.azurewebsites.net/');
 
 // DIR
-define('DIR_APPLICATION', 'C:/OpenServer_5.2.6/OpenServer/domains/netpingdevice.localhost/NetPing_OpenCart/admin/');
-define('DIR_SYSTEM', 'C:/OpenServer_5.2.6/OpenServer/domains/netpingdevice.localhost/NetPing_OpenCart/system/');
-define('DIR_IMAGE', 'C:/OpenServer_5.2.6/OpenServer/domains/netpingdevice.localhost/NetPing_OpenCart/image/');
-define('DIR_LANGUAGE', 'C:/OpenServer_5.2.6/OpenServer/domains/netpingdevice.localhost/NetPing_OpenCart/admin/language/');
-define('DIR_TEMPLATE', 'C:/OpenServer_5.2.6/OpenServer/domains/netpingdevice.localhost/NetPing_OpenCart/admin/view/template/');
-define('DIR_CONFIG', 'C:/OpenServer_5.2.6/OpenServer/domains/netpingdevice.localhost/NetPing_OpenCart/system/config/');
-define('DIR_CACHE', 'C:/OpenServer_5.2.6/OpenServer/domains/netpingdevice.localhost/NetPing_OpenCart/system/storage/cache/');
-define('DIR_DOWNLOAD', 'C:/OpenServer_5.2.6/OpenServer/domains/netpingdevice.localhost/NetPing_OpenCart/system/storage/download/');
-define('DIR_LOGS', 'C:/OpenServer_5.2.6/OpenServer/domains/netpingdevice.localhost/NetPing_OpenCart/system/storage/logs/');
-define('DIR_MODIFICATION', 'C:/OpenServer_5.2.6/OpenServer/domains/netpingdevice.localhost/NetPing_OpenCart/system/storage/modification/');
-define('DIR_UPLOAD', 'C:/OpenServer_5.2.6/OpenServer/domains/netpingdevice.localhost/NetPing_OpenCart/system/storage/upload/');
-define('DIR_CATALOG', 'C:/OpenServer_5.2.6/OpenServer/domains/netpingdevice.localhost/NetPing_OpenCart/catalog/');
+define('DIR_APPLICATION', 'D:/home/site/wwwroot/admin/');
+define('DIR_SYSTEM', 'D:/home/site/wwwroot/system/');
+define('DIR_IMAGE', 'D:/home/site/wwwroot/image/');
+define('DIR_LANGUAGE', 'D:/home/site/wwwroot/admin/language/');
+define('DIR_TEMPLATE', 'D:/home/site/wwwroot/admin/view/template/');
+define('DIR_CONFIG', 'D:/home/site/wwwroot/system/config/');
+define('DIR_CACHE', 'D:/home/site/wwwroot/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/home/site/wwwroot/system/storage/download/');
+define('DIR_LOGS', 'D:/home/site/wwwroot/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/home/site/wwwroot/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/home/site/wwwroot/system/storage/upload/');
+define('DIR_CATALOG', 'D:/home/site/wwwroot/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
+define('DB_HOSTNAME', 'eu-cdbr-azure-west-d.cloudapp.net');
 define('DB_USERNAME', 'be570f71559f78');
 define('DB_PASSWORD', '3e99587e');
 define('DB_DATABASE', 'netpingopencartdb');
